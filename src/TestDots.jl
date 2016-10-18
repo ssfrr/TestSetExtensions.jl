8697e85f5721ce6dbd70ba8d143cf56a4247493f
Spencer Russell <spencer.f.russell@gmail.com>
2016-10-18 00:56:37 -0400

    adds DottedTestSet implementation

--- src/TestDots.jl
+++ src/TestDots.jl
@@ -1,5 +1,39 @@
 module TestDots
 
-# package code goes here
+export DottedTestSet
+
+if VERSION >= v"0.5.0-dev+7720"
+    using Base.Test
+    import Base.Test: record, finish
+    using Base.Test: DefaultTestSet, AbstractTestSet, Pass, get_testset_depth
+else
+    using BaseTestNext
+    import BaseTestNext: record, finish
+    using BaseTestNext: DefaultTestSet, AbstractTestSet, Pass, get_testset_depth
+end
+
+
+type DottedTestSet{T<:AbstractTestSet} <: AbstractTestSet
+    wrapped::T
+
+    DottedTestSet(desc) = new(T(desc))
+end
+
+function DottedTestSet(desc; wrap=DefaultTestSet)
+    DottedTestSet{wrap}(desc)
+end
+
+function record(ts::DottedTestSet, res::Pass)
+    print_with_color(:green, ".")
+    record(ts.wrapped, res)
+    res
+end
+
+record(ts::DottedTestSet, res) = record(ts.wrapped, res)
+
+function finish(ts::DottedTestSet)
+    get_testset_depth() == 0 && print("\n\n")
+    finish(ts.wrapped)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,29 @@
 using TestDots
-using Base.Test
+using Suppressor
 
-# write your own tests here
-@test 1 == 2
+if VERSION >= v"0.5.0-dev+7720"
+    using Base.Test
+else
+    using BaseTestNext
+end
+
+output = @capture_out begin
+    @testset DottedTestSet "top-level tests" begin
+        @testset "2nd-level tests 1" begin
+            @test true
+            @test 1 == 1
+        end
+        @testset "2nd-level tests 2" begin
+            @test true
+            @test 1 == 1
+        end
+    end
+end
+
+@testset "check output" begin
+    @test output == """
+....
+
+Test Summary:   | Pass  Total
+  top-level tests |    4      4\n"""
+end
