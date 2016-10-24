ee8f4c97b30ab844e31a2a00e48720d5a73d9620
Spencer Russell <spencer.f.russell@gmail.com>
2016-10-24 09:35:51 -0400

    now testing both color and nocolor conditions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using DottedTestSets
+using TestSetExtensions
 using Suppressor
 
 if VERSION >= v"0.5.0-dev+7720"
@@ -7,7 +7,10 @@ else
     using BaseTestNext
 end
 
-output = @capture_out begin
+orig_color = Base.have_color
+
+eval(Base, :(have_color = true))
+output_color = @capture_out begin
     @testset DottedTestSet "top-level tests" begin
         @testset "2nd-level tests 1" begin
             @test true
@@ -20,10 +23,57 @@ output = @capture_out begin
     end
 end
 
-@testset "check output" begin
-    @test output == """
-....
+eval(Base, :(have_color = false))
+output_nocolor = @capture_out begin
+    @testset DottedTestSet "top-level tests" begin
+        @testset "2nd-level tests 1" begin
+            @test true
+            @test 1 == 1
+        end
+        @testset "2nd-level tests 2" begin
+            @test true
+            @test 1 == 1
+        end
+    end
+end
 
-Test Summary:   | Pass  Total
-  top-level tests |    4      4\n"""
+eval(Base, :(have_color = $orig_color))
+
+@testset DottedTestSet "TextSetExtensions Tests" begin
+    @testset "check output" begin
+        @test output_color ==
+            """\e[1m\e[32m.\e[0m\e[1m\e[32m.\e[0m\e[1m\e[32m.\e[0m\e[1m\e[32m.\e[0m
+
+            \e[1m\e[37mTest Summary:   | \e[0m\e[1m\e[32mPass  \e[0m\e[1m\e[34mTotal\e[0m
+              top-level tests | \e[1m\e[32m   4  \e[0m\e[1m\e[34m    4\e[0m
+            """
+
+        @test output_nocolor ==
+            """
+            ....
+
+            Test Summary:   | Pass  Total
+              top-level tests |    4      4
+            """
+    end
+
+  # @testset "Auto-run test files" begin
+  #   file1_run = false
+  #   file2_run = false
+  #
+  #   runtests()
+  #
+  #   @test file1_run
+  #   @test file2_run
+  # end
+  #
+  # @testset "run selected test files" begin
+  #   file1_run = false
+  #   file2_run = false
+  #
+  #   runtests(["file1"])
+  #
+  #   @test file1_run
+  #   @test !file2_run
+  # end
 end
